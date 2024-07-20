70c8366f1d94fd9960addbd9f0cfc5c95561b7f1
bphkns <bphkns@gmail.com>
2024-07-20 21:52:30 +0530

    add

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,7 +14,6 @@ return {
             -- Look for nx.json in parent directories
             return util.root_pattern("nx.json")(fname) or util.root_pattern("angular.json", "project.json")(fname)
           end,
-          filetypes = { "angular", "html", "typescript", "typescriptreact", "typescript.tsx", "htmlangular" },
         },
         vtsls = {
           settings = {
@@ -35,39 +34,13 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      local util = require("lspconfig.util")
-      local function get_angular_ls_path()
-        local workspace_root = util.root_pattern("nx.json")(vim.fn.getcwd())
-        if workspace_root then
-          return workspace_root .. "/node_modules/@angular/language-server"
-        end
-        return nil
-      end
-
-      local angular_ls_path = get_angular_ls_path()
-      if angular_ls_path then
-        opts.servers.angularls = opts.servers.angularls or {}
-        opts.servers.angularls.cmd = {
-          "ngserver",
-          "--stdio",
-          "--tsProbeLocations",
-          angular_ls_path .. "/node_modules",
-          "--ngProbeLocations",
-          angular_ls_path .. "/node_modules",
-        }
-        LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-          {
-            name = "@angular/language-server",
-            location = angular_ls_path,
-            enableForWorkspaceTypeScriptVersions = false,
-          },
-        })
-      else
-        vim.notify(
-          "Could not find Angular Language Server. Make sure it's installed in your project.",
-          vim.log.levels.WARN
-        )
-      end
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@angular/language-server",
+          location = LazyVim.get_pkg_path("angular-language-server", "/node_modules/@angular/language-server"),
+          enableForWorkspaceTypeScriptVersions = false,
+        },
+      })
     end,
     setup = {
       angularls = function()
