405f8bb9cc9207f0952fcce5169ffa7133edbcd7
bphkns <bphkns@gmail.com>
2024-07-28 06:33:55 +0530

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,14 +9,6 @@ return {
         "emmet_ls",
       },
       servers = {
-        angularls = {
-          root_dir = function(fname)
-            local util = require("lspconfig.util")
-            -- Look for nx.json in parent directories
-            return util.root_pattern("angular.json", "nx.json")(fname)
-          end,
-          filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
-        },
         vtsls = {
           settings = {
             complete_function_calls = false,
@@ -30,45 +22,61 @@ return {
         emmet_ls = {
           filetypes = {
             "astro",
-            "css",
-            "eruby",
             "html",
-            "htmldjango",
-            "javascriptreact",
-            "less",
-            "pug",
-            "sass",
-            "scss",
-            "svelte",
+            "typescript",
             "typescriptreact",
-            "vue",
+            "typescript.jsx",
             "htmlangular",
+            "css",
+            "javascript",
+            "javascriptreact",
+            "scss",
+            "sass",
           },
         },
-      },
-      inlay_hints = {
-        enabled = false,
-      },
-      setup = {
-        angularls = function()
-          LazyVim.lsp.on_attach(function(client)
-            --HACK: disable angular renaming capability due to duplicate rename popping up
-            client.server_capabilities.renameProvider = false
-          end, "angularls")
-        end,
       },
     },
   },
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-        {
-          name = "@angular/language-server",
-          location = LazyVim.get_pkg_path("angular-language-server", "/node_modules/@angular/language-server"),
-          enableForWorkspaceTypeScriptVersions = false,
-        },
-      })
+      local util = require("lspconfig.util")
+      local function get_angular_ls_path()
+        local workspace_root = util.root_pattern("nx.json")(vim.fn.getcwd())
+        if workspace_root then
+          return workspace_root .. "/node_modules/@angular/language-server"
+        end
+        return nil
+      end
+
+      local angular_ls_path = get_angular_ls_path()
+      if angular_ls_path then
+        opts.servers.angularls = opts.servers.angularls or {}
+        opts.servers.angularls.root_dir = function(fname)
+          return util.root_pattern("angular.json", "nx.json")(fname)
+        end
+        opts.servers.angularls.filetypes = { "angular", "html", "typescript", "typescriptreact", "htmlangular" }
+        LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+          {
+            name = "@angular/language-server",
+            location = angular_ls_path,
+            enableForWorkspaceTypeScriptVersions = false,
+          },
+        })
+      else
+        vim.notify(
+          "Could not find Angular Language Server. Make sure it's installed in your project.",
+          vim.log.levels.WARN
+        )
+      end
     end,
+    setup = {
+      angularls = function()
+        LazyVim.lsp.on_attach(function(client)
+          --HACK: disable angular renaming capability due to duplicate rename popping up
+          client.server_capabilities.renameProvider = false
+        end, "angularls")
+      end,
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,27 @@
 return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "angular",
+        "tsx",
+        "typescript",
+        "html",
+        "yaml",
+        "json",
+        "css",
+        "scss",
+        "sql",
+        "dockerfile",
+        "prisma",
+      })
+    end,
+  },
   { "nvim-treesitter/playground" },
   {
     "nvim-treesitter",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, {
-          "angular",
-          "tsx",
-          "typescript",
-          "html",
-          "yaml",
-          "json",
-          "css",
-          "scss",
-          "sql",
-          "dockerfile",
-          "prisma",
-        })
-      end
       vim.filetype.add({
         pattern = {
           [".*%.component%.html"] = "htmlangular", -- Sets the filetype to `htmlangular` if it matches the pattern
