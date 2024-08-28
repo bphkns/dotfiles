9cafd0ae268c3efee52729c0ad2e56c9e2280fcf
Bikash Phukan <bikash@spotdraft.com>
2024-08-28 11:41:47 +0530

    a

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
-
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
 config.font_size = 14
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.color_scheme = "Catppuccin Macchiato"
+config.colors = theme.colors()
 return config
