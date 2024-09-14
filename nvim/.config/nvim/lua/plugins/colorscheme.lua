9cd055fd3d01c2a7fa450c738b85007dd242eec1
bphkns <bphkns@gmail.com>
2024-09-15 00:14:35 +0530

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,65 +1,77 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
+    "LazyVim/LazyVim",
     opts = {
-      style = "moon",
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.LspReferenceText = {
-          bg = "none",
-        }
-      end,
+      colorscheme = "catppuccin",
     },
   },
   {
-    "EdenEast/nightfox.nvim",
-    name = "nightfox",
-    lazy = false,
-    priority = 1000,
+    "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
-      groups = {
+      term_colors = true,
+      transparent_background = false,
+      flavour = "mocha",
+      highlight_overrides = {
         all = {
-          LspReferenceText = {
-            bg = "none",
+          LspReferenceText = { bg = "NONE" },
+        },
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
           },
         },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
       },
     },
   },
   {
-    "LazyVim/LazyVim",
-    lazy = false,
+    "akinsho/bufferline.nvim",
+    after = "catppuccin",
     opts = {
-      colorscheme = "carbonfox",
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    after = "catppuccin",
+    opts = {
+      options = {
+        theme = "catppuccin",
+      },
     },
   },
 }
