491126b4431207dc3766db8d4a5d9f843ef10e8c
bphkns <bphkns@gmail.com>
2024-09-19 00:04:29 +0530

    add

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,77 +1,33 @@
 return {
   {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+        functions = {},
+        variables = {},
+      },
+      gamma = 0.8,
+      custom_highlights = function(hl)
+        hl.LspReferenceText = {
+          bg = "none",
+        }
+        hl["@text.emphasis"] = {
+          italic = false,
+        }
+
+        return {}
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      term_colors = true,
-      transparent_background = true,
-      flavour = "mocha",
-      highlight_overrides = {
-        all = {
-          LspReferenceText = { bg = "NONE" },
-        },
-      },
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = {
-          enabled = true,
-          style = "nvchad",
-        },
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    after = "catppuccin",
-    opts = {
-      highlights = require("catppuccin.groups.integrations.bufferline").get(),
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    after = "catppuccin",
-    opts = {
-      options = {
-        theme = "catppuccin",
-      },
+      colorscheme = "tokyodark",
     },
   },
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
-config.color_scheme = "Dark+"
+config.font = wezterm.font("CaskaydiaCove Nerd Font", {
+	weight = "Light",
+})
+config.color_scheme = "tokyodark"
 config.font_size = 14
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
