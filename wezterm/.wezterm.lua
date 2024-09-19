09b8399ffcc6cf6157a100e28bc1439cc8111012
Bikash Phukan <bikash@spotdraft.com>
2024-09-19 13:22:44 +0530

    update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font", {
-	weight = "Light",
-})
+config.font = wezterm.font("CaskaydiaCove Nerd Font", {})
 config.color_scheme = "tokyodark"
 config.font_size = 14
 config.enable_tab_bar = false
