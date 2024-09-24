ac036d4a0f40c7340ac006c74650fcfd74e6e26c
Bikash Phukan <bikash@spotdraft.com>
2024-09-24 20:24:17 +0530

    add

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font = wezterm.font("IosevkaTerm Nerd Font")
 config.font_size = 14
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
