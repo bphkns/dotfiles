27d849a1d7610fefe21abb1142f14a75e2c582bc
Bikash Phukan <bikash@spotdraft.com>
2025-01-30 11:34:01 +0530

    add

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-material",
-      background = "dark",
-      gruvbox_material_background = "hard",
-    },
-  },
+  --   {
+  --     "sainnhe/gruvbox-material",
+  --     lazy = false,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "LazyVim/LazyVim",
+  --     opts = {
+  --       colorscheme = "gruvbox-material",
+  --       background = "dark",
+  --       gruvbox_material_background = "hard",
+  --     },
+  --   },
 }
