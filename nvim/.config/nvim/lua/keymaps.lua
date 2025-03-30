b19aee428f43a195722c83badda3c2b50b1c4f79
bphkns <bphkns@gmail.com>
2025-03-30 18:46:04 +0530

    add

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,12 +5,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 
 -- Keep cursor centered on half-page jumping
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 
 -- Keep search terms in the middle when browsing results
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
 -- Yank to clipboard
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank to clipboard" })
@@ -20,13 +20,16 @@ vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank to clipboard" })
 vim.keymap.set("n", "<leader>D", '"_d', { desc = "[D]elete into void" })
 
 -- Don't press M (tags)
-vim.keymap.set("n", "<C-m>", "<nop>")
+vim.keymap.set("n", "<C-m>", "<nop>", opts)
 
 -- Don't press Q
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>", opts)
 
 -- Delete all buffers except the current one and return to previous mark
 vim.keymap.set("n", "<leader>bsd", "<cmd>%bd|e#|bd#<cr>|'<cr>", { desc = "Delete surrounding buffers" })
+
+-- Add the following line to your init.lua
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })
 
 -- Search and replace word under cursor
 vim.keymap.set(
@@ -43,7 +46,7 @@ vim.keymap.set("n", "<LEFT>", "<NOP>", opts)
 vim.keymap.set("n", "<RIGHT>", "<NOP>", opts)
 
 -- Display current filetype
-vim.keymap.set({ "n", "t", "v", "i", "" }, "<C-x>", "<cmd>echo &filetype<cr>", opts)
+vim.keymap.set({ "n", "t", "v", "i" }, "<C-x>", "<cmd>echo &filetype<cr>", opts)
 
 -- Paste without yanking the replaced text
 vim.keymap.set("v", "p", '"_dP', opts)
