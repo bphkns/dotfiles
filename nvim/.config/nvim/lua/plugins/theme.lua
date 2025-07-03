9ef0bbe4b98cb0ffa8d9459d4fca66f3a561f40d
bphkns <bphkns@gmail.com>
2025-07-04 01:07:14 +0530

    fix colors and themes

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,31 +1,31 @@
 return {
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("rose-pine").setup({
-  --       variant = "moon",
-  --       dark_variant = "moon",
-  --       styles = {
-  --         bold = true,
-  --         italic = false,
-  --         transparency = false,
-  --       },
-  --     })
-  --     vim.cmd("colorscheme rose-pine")
-  --   end,
-  -- },
   {
-    "olimorris/onedarkpro.nvim",
-    name = "onedarkpro",
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
     lazy = false,
+    priority = 1000,
     config = function()
-      require("onedarkpro").setup({
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
       })
-      vim.cmd("colorscheme onedark_vivid")
+      vim.cmd("colorscheme rose-pine")
     end,
-  }
+  },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   name = "onedarkpro",
+  --   priority = 1000,
+  --   lazy = false,
+  --   config = function()
+  --     require("onedarkpro").setup({
+  --     })
+  --     vim.cmd("colorscheme vaporwave")
+  --   end,
+  -- }
 }
