19532bc9c22de98c1ed9e5d49ba7e6c7fc122ecf
Bikash Phukan <bikash@spotdraft.com>
2025-09-01 13:12:03 +0530

    ai

--- nvim/.config/nvim/lua/plugins/codecompaninon.lua
+++ nvim/.config/nvim/lua/plugins/codecompaninon.lua
@@ -1,4 +1,42 @@
 return {
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" }
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("mcphub").setup({
+        config = vim.fn.expand("~/.config/mcphub/servers.json"),
+      })
+    end
+  },
   {
     "olimorris/codecompanion.nvim",
     event = "VeryLazy",
@@ -6,15 +44,31 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    opts = {},
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            -- MCP Tools
+            make_tools = true,                    -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+            show_server_tools_in_chat = true,     -- Show individual tools in chat completion (when make_tools=true)
+            add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+            show_result_in_chat = true,           -- Show tool results directly in chat buffer
+            format_tool = nil,                    -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+            -- MCP Resources
+            make_vars = true,                     -- Convert MCP resources to #variables for prompts
+            -- MCP Prompts
+            make_slash_commands = true,
+          }
+        }
+      }
+    },
     config = function(_, opts)
       require("codecompanion").setup(opts)
       vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanion<cr>", { noremap = true, silent = true })
       vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>",
         { noremap = true, silent = true })
       vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
-
-      -- Expand 'cc' into 'CodeCompanion' in the command line
       vim.cmd([[cab cc CodeCompanion]])
     end
   },
